2c3e51a2f95542527a7e51b032e37d59ee45f0fb
Martoko <Martoko@users.noreply.github.com>
2015-10-19 10:43:01 +0200

    Setup initial UI

---
+++ Music Voter UI/JoinServerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JoinServerTableViewController.h
+//  Music Voter UI
+//
+//  Created by Martoko on 19/10/15.
+//  Copyright © 2015 Mathias & Magnus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoinServerTableViewController : UITableViewController
+
+@end
